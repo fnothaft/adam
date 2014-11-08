1d64e5ea7a63b84476cb52ef105f645359f5d33c
Eric Tu <erictctu@gmail.com>
2014-11-08 13:48:55 -0800

    works with rebase

--- adam-cli/src/main/scala/org/bdgenomics/adam/cli/VizReads.scala
+++ adam-cli/src/main/scala/org/bdgenomics/adam/cli/VizReads.scala
@@ -25,9 +25,8 @@ import org.bdgenomics.adam.models.{ OrderedTrackedLayout, ReferenceRegion }
 import org.bdgenomics.adam.projections.AlignmentRecordField._
 import org.bdgenomics.adam.projections.Projection
 import org.bdgenomics.adam.rdd.ADAMContext._
-import org.bdgenomics.adam.rdd.read.ADAMAlignmentRecordContext._
-import org.bdgenomics.adam.rdd.variation.ADAMVariationContext._ //to do the filtering by
-
+import org.bdgenomics.adam.rdd.read.AlignmentRecordContext._
+import org.bdgenomics.adam.rdd.variation.VariationContext._ //to do the filtering by
 import org.bdgenomics.adam.rich.ReferenceMappingContext.AlignmentRecordReferenceMapping
 import org.bdgenomics.formats.avro.{ AlignmentRecord, Genotype }
 import org.fusesource.scalate.TemplateEngine
