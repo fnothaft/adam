f734a78912bc4fa981da799bc570a9eeb7368b2c
Frank Austin Nothaft <fnothaft@alumni.stanford.edu>
2015-12-23 14:37:31 -0800

    [ADAM-905] Move to bdg-formats 0.7.0. Resolves #905.

--- adam-core/src/main/scala/org/bdgenomics/adam/models/ReadBucket.scala
+++ adam-core/src/main/scala/org/bdgenomics/adam/models/ReadBucket.scala
@@ -96,17 +96,17 @@ class ReadBucketSerializer extends Serializer[ReadBucket] {
 object ReadBucket {
   implicit def singleReadBucketToReadBucket(bucket: SingleReadBucket): ReadBucket = {
     // check that reads are either first or second read from fragment
-    bucket.primaryMapped.foreach(r => require(r.getReadNum >= 0 && r.getReadNum <= 1,
-      "Read %s is not first or second read from pair (num = %d).".format(r, r.getReadNum)))
-    bucket.secondaryMapped.foreach(r => require(r.getReadNum >= 0 && r.getReadNum <= 1,
-      "Read %s is not first or second read from pair (num = %d).".format(r, r.getReadNum)))
-    bucket.unmapped.foreach(r => require(r.getReadNum >= 0 && r.getReadNum <= 1,
-      "Read %s is not first or second read from pair (num = %d).".format(r, r.getReadNum)))
+    bucket.primaryMapped.foreach(r => require(r.getReadInFragment >= 0 && r.getReadInFragment <= 1,
+      "Read %s is not first or second read from pair (num = %d).".format(r, r.getReadInFragment)))
+    bucket.secondaryMapped.foreach(r => require(r.getReadInFragment >= 0 && r.getReadInFragment <= 1,
+      "Read %s is not first or second read from pair (num = %d).".format(r, r.getReadInFragment)))
+    bucket.unmapped.foreach(r => require(r.getReadInFragment >= 0 && r.getReadInFragment <= 1,
+      "Read %s is not first or second read from pair (num = %d).".format(r, r.getReadInFragment)))
 
     val (pairedPrimary, unpairedPrimary) = bucket.primaryMapped.partition(_.getReadPaired)
-    val (pairedFirstPrimary, pairedSecondPrimary) = pairedPrimary.partition(_.getReadNum == 0)
+    val (pairedFirstPrimary, pairedSecondPrimary) = pairedPrimary.partition(_.getReadInFragment == 0)
     val (pairedSecondary, unpairedSecondary) = bucket.secondaryMapped.partition(_.getReadPaired)
-    val (pairedFirstSecondary, pairedSecondSecondary) = pairedSecondary.partition(_.getReadNum == 0)
+    val (pairedFirstSecondary, pairedSecondSecondary) = pairedSecondary.partition(_.getReadInFragment == 0)
 
     new ReadBucket(unpairedPrimary,
       pairedFirstPrimary,
